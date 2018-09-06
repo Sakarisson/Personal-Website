8d1af3d4c273b8828eae30349de38081cd7753fb
Kristian Sakarisson <kristian@sakarisson.com>
2018-09-06 09:07:51 +0300

    Deleted demo stuff

--- src/client/index.js
+++ src/client/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import Store from 'Store';
 
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
 
-import Information from 'Components/PersonInformation';
-
-const instance = new Store();
-
-const App = ({ store }) => (
+const App = () => (
   <div>
     Welcome to React, Webpack4, Mobx, Express starter kit.
-    <Information person={store.person} />
   </div>
 );
 
-App.propTypes = {
-  store: PropTypes.instanceOf(Store).isRequired,
-};
-
-ReactDOM.render(<App store={instance} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
