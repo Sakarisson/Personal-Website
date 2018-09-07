d59a1ec2941dfa5365a6be3a98e5061d645cf571
Kristian Sakarisson <kristian.sakarisson@druid.fi>
2018-09-07 14:47:08 +0300

    Added google-analytics

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import 'babel-polyfill';
 
 import Home from 'Pages/Home';
@@ -10,5 +12,6 @@ const App = () => (
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize('UA-85011695-2');
 
 export default App;
