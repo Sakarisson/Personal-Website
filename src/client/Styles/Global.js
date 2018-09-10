74c068ffd2b1e7b4723d0f6e10aad8359652194d
Kristian Sakarisson <kristian.sakarisson@druid.fi>
2018-09-10 10:10:39 +0300

    Refactored to css-grid

---
+++ src/client/Styles/Global.js
@@ -0,0 +1,11 @@
+import { injectGlobal } from 'styled-components';
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  html, body {
+    height: 100%;
+    width: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+`;
--- src/client/index.js
+++ src/client/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
+import styled from 'styled-components';
+import 'Styles/Global';
 
 import 'babel-polyfill';
 
 import Home from 'Pages/Home';
 
+const Root = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 570px 1fr;
+  grid-template-rows: 1fr 100px 2fr;
+
+  @media (max-width: 570px) {
+    grid-template-columns: 5% 90% 5%;
+  }
+
+  grid-template-areas:
+    "header header header"
+    "left middle right"
+    "footer footer footer"
+`;
+
 const App = () => (
-  <Home />
+  <Root>
+    <Home />
+  </Root>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
